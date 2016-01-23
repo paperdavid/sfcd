d1ebb9dcfce2cda0a60c98481a807d7ffd147e56
David <paperdavid@gmail.com>
2016-01-23 15:58:46 +0300

    trademarks+stores

--- src/js/custom.js
+++ src/js/custom.js
@@ -6,6 +6,23 @@ $(document).ready(function() {
 
     /* Custom */
 
-    
+  $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:false,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:1
+          },
+          600:{
+              items:2
+          },
+          1000:{
+              items:3,
+              loop:false
+          }
+      }
+  })
 
 });
